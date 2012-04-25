996952c00bf1eb4de3fe81b6f4d66e3b8097ba29
Zach Moazeni <zach@collectiveidea.com>
2012-04-25 19:41:50 -0400

    Implementing the enqueue action for the server

--- delayed_job/server.rb
+++ delayed_job/server.rb
@@ -1,7 +1,12 @@
 module DelayedJob
   class Server < Sinatra::Base
-    get "/foo" do
-      "Hullo there!"
+    post "/enqueue" do
+      request.body.rewind
+      job = MultiJson.load(request.body)
+      request.body.rewind
+
+      Qu.enqueue(job["klass"], job["args"])
+      [202, {"Content-Type" => "application/json"}, ""]
     end
   end
 end
